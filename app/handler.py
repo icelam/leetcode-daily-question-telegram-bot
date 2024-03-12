0106e09eedc980222b80e74d084ae8355108e6ec
Ice Lam <pinky.yy.lam@gmail.com>
2024-03-12 18:04:07 +0800

    feat: remove hard-coded user agent string which is unnecessary to fire a successful call

--- app/handler.py
+++ app/handler.py
@@ -45,7 +45,6 @@ def get_question_of_today():
         },
         headers={
             "authority": "leetcode.com",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         }
     )
 
