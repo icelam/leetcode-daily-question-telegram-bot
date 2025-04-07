480fcb9fa82a0f6e59fe365f8cb6d425276d06fb
Ice Lam <pinky.yy.lam@gmail.com>
2025-04-07 10:47:00 +0800

    fix(markdown): strip style tags when converting html to markdown

--- app/utils/markdown.py
+++ app/utils/markdown.py
@@ -55,12 +55,15 @@ class TelegramMarkdownConverter(MarkdownConverter):
             convert_as_inline=convert_as_inline
         )
 
+    def convert_style(self, el, text, convert_as_inline):
+        return ''
+
 def generate(html, **options):
     """Convert function with options predefined"""
 
     result = TelegramMarkdownConverter(
         **options,
-        convert=['br', 'p', 'img', 'code', 'pre', 'ul', 'ol', 'li', 'a', 'sup', 'sub'],
+        convert=['br', 'p', 'img', 'code', 'pre', 'ul', 'ol', 'li', 'a', 'sup', 'sub', 'style'],
         bullets='•••'
     ).convert(html).strip()
 
